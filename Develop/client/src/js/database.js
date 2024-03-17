7564cf0695d19d87026fc1cccc25b95b0a61bbc1
bladeasf <125907788+bladeasf@users.noreply.github.com>
2024-03-17 00:41:01 -0400

    database.js

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -1,21 +1,45 @@
-import { openDB } from 'idb';
-
+import { openDB } from "idb";
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  try {
+    const db = await openDB("jate", 1);
+    const tx = db.transaction("jate", "readwrite");
+    const store = tx.objectStore("jate");
+    const request = store.add({ content });
+    const result = await request;
+  } catch (error) {
+    console.error("putDb not implemented", error);
+  }
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  try {
+    console.log("GET from the database");
 
+    const db = await openDB("jate", 1);
+    const tx = db.transaction("jate", "readonly");
+    const store = tx.objectStore("jate");
+    const request = store.getAll();
+    const result = await request;
+    const position = result.length - 1;
+    if (result.length === 0) {
+      return null;
+    } else {
+      return result[position].content;
+    }
+  } catch (error) {
+    console.error("getDb not implemented", error);
+  }
+};
 initdb();
